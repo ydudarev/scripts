1953202dd09f1765641327ed845de526ca3ebf0d
MrWise <74302293+ydudarev@users.noreply.github.com>
2020-12-18 14:27:22 +0300

    Add files via upload

---
+++ proxie.cmd
@@ -0,0 +1,6 @@
+@Echo off
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "MigrateProxy" /t REG_DWORD /d 00000001 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" /t REG_DWORD /d 00000001 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyServer" /t REG_SZ /d 10.0.0.103:80 /f
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main" /v "Start Page" /t REG_SZ /d http://yandex.ru/ /f

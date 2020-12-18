effb0a878da1f8e5fca987aa45e4ed7c655e4e86
MrWise <74302293+ydudarev@users.noreply.github.com>
2020-12-18 15:24:01 +0300

    Update proxie.cmd

--- proxie.cmd
+++ proxie.cmd
@@ -3,4 +3,4 @@
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "MigrateProxy" /t REG_DWORD /d 00000001 /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyEnable" /t REG_DWORD /d 00000001 /f
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyServer" /t REG_SZ /d 10.77.44.102:8080 /f
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyOverride" /t REG_SZ /d "10.77.*.*;*.tech.nis-glonass.ru"
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "ProxyOverride" /t REG_SZ /d "10.77.*.*;*.tech.nis-glonass.ru" /f      
